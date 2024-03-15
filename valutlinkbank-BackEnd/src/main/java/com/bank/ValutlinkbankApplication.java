66db3243e3fbfe2dbb86d26c35d5b0c3985a1eb5
PREETHIYAASH <preethiyashmi28@gmail.com>
2024-03-15 11:09:18 +0530

    valuelink

---
+++ valutlinkbank-BackEnd/src/main/java/com/bank/ValutlinkbankApplication.java
@@ -0,0 +1,14 @@
+package com.bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication                                               
+public class ValutlinkbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValutlinkbankApplication.class, args);
+		System.out.println("Welcome to valutlinkbank");
+	}
+
+}
